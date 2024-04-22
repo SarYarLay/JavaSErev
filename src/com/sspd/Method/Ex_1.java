7b151a38a1ba22133e329a079880954fc3bc2f77
Maung Naing <mgnaingterry@gamil.com>
2024-04-22 04:09:07 -0700

    update

--- src/com/sspd/Method/Ex_1.java
+++ src/com/sspd/Method/Ex_1.java
@@ -2,7 +2,24 @@ package com.sspd.Method;
 
 public class Ex_1 {
 
+    public static void main(String[] args) {
 
+       new Ex_1().getInfo();
+
+       getPrint();
+
+    }
+
+    void getInfo(){
+        System.out.println("I am Developer");
+
+
+
+    }
+    static void getPrint(){
+        System.out.println("I am Mg Naing");
+
+    }
 
 
 

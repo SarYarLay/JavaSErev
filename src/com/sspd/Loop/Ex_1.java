deb4c093bb30bf25a53d5541434a590d4e27b2a8
Maung Naing <mgnaingterry@gamil.com>
2024-04-23 00:50:23 -0700

    update

---
+++ src/com/sspd/Loop/Ex_1.java
@@ -0,0 +1,16 @@
+package com.sspd.Loop;
+
+public class Ex_1 {
+    public static void main(String[] args) {
+
+
+        for(char i='A';i<='E';i++){
+
+            System.out.println(i);
+
+        }
+
+    }
+
+
+}
